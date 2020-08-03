d7da5b2c99ad5e75e2592b436594a303addb058c
Travis CI <travis@travis-ci.org>
2020-08-03 21:47:44 +0000

    Deploy code docs to GitHub Pages Travis build: 155
    Commit: 09ad99d1512873ddd6ba1784a2477c4c28055242

--- docs/develop-wip/doc/html/search/all_15.js
+++ docs/develop-wip/doc/html/search/all_15.js
@@ -1,11 +1,17 @@
 var searchData=
 [
-  ['_7edomaingenerator',['~DomainGenerator',['../classThunderEgg_1_1DomainGenerator.html#a4fb66e17eaa5b43282f59ead2e70ded4',1,'ThunderEgg::DomainGenerator']]],
+  ['_7edomaingenerator',['~DomainGenerator',['../classThunderEgg_1_1DomainGenerator.html#af1fec1ab0d5e09c5996a6ca336ba6c0a',1,'ThunderEgg::DomainGenerator']]],
+  ['_7eifaceinfo',['~IfaceInfo',['../classThunderEgg_1_1Schur_1_1IfaceInfo.html#ac8ae6917b6ec2b8e29414b8584352e27',1,'ThunderEgg::Schur::IfaceInfo']]],
   ['_7einterlevelcomm',['~InterLevelComm',['../classThunderEgg_1_1GMG_1_1InterLevelComm.html#a975b14015ff76fd3677e0484cb9240f0',1,'ThunderEgg::GMG::InterLevelComm']]],
+  ['_7einterpolator',['~Interpolator',['../classThunderEgg_1_1GMG_1_1Interpolator.html#acbb09ada4eac8e0554e3a7aba8ad1501',1,'ThunderEgg::GMG::Interpolator']]],
   ['_7enbrinfo',['~NbrInfo',['../classThunderEgg_1_1NbrInfo.html#afbc8023c274d906987aada0239822044',1,'ThunderEgg::NbrInfo']]],
+  ['_7eoperator',['~Operator',['../classThunderEgg_1_1Operator.html#ab39411e3930fc78b6fa990c63b7b3047',1,'ThunderEgg::Operator']]],
   ['_7epatchoperator',['~PatchOperator',['../classThunderEgg_1_1PatchOperator.html#a944b92b49d15750cb796ce6589be1b7b',1,'ThunderEgg::PatchOperator']]],
   ['_7epatchsolver',['~PatchSolver',['../classThunderEgg_1_1PatchSolver.html#a83c9b911317e4e29e7ec0182d5796ff2',1,'ThunderEgg::PatchSolver::~PatchSolver()'],['../classThunderEgg_1_1Schur_1_1PatchSolver.html#aa16b9e60efe11b85233ecdb94bad9dc4',1,'ThunderEgg::Schur::PatchSolver::~PatchSolver()']]],
   ['_7epetscvector',['~PetscVector',['../classThunderEgg_1_1PetscVector.html#ab97eeaa2af6d666a7551503be2dd2f69',1,'ThunderEgg::PetscVector']]],
   ['_7epw',['~PW',['../classThunderEgg_1_1PW.html#ad023505fad5487594f2b57e174310216',1,'ThunderEgg::PW']]],
+  ['_7erestrictor',['~Restrictor',['../classThunderEgg_1_1GMG_1_1Restrictor.html#afe424ba528fe3aa85c26b4103c8dcc11',1,'ThunderEgg::GMG::Restrictor']]],
+  ['_7eserializable',['~Serializable',['../classThunderEgg_1_1Serializable.html#a3b39aed7f11caf84cda71c529eae1460',1,'ThunderEgg::Serializable']]],
+  ['_7esmoother',['~Smoother',['../classThunderEgg_1_1GMG_1_1Smoother.html#a658ca92bced694154e23f7a8669941b0',1,'ThunderEgg::GMG::Smoother']]],
   ['_7evector',['~Vector',['../classThunderEgg_1_1Vector.html#a82611c6436f802530cf5147681edc645',1,'ThunderEgg::Vector']]]
 ];
