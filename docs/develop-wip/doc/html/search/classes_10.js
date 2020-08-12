9a9cfdd1d865206ea5d2fed8cfbf7dc4d8d6a5ce
Travis CI <travis@travis-ci.org>
2020-08-12 21:48:34 +0000

    Deploy code docs to GitHub Pages Travis build: 188
    Commit: 66236ce30630210a3b0ecb10004c260a4cccb6b4

--- docs/develop-wip/doc/html/search/classes_10.js
+++ docs/develop-wip/doc/html/search/classes_10.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['timer',['Timer',['../classThunderEgg_1_1Timer.html',1,'ThunderEgg']]],
-  ['timerexception',['TimerException',['../structThunderEgg_1_1TimerException.html',1,'ThunderEgg']]],
   ['tree',['Tree',['../structThunderEgg_1_1Experimental_1_1Tree.html',1,'ThunderEgg::Experimental']]],
   ['trilininterp',['TriLinInterp',['../classThunderEgg_1_1Schur_1_1TriLinInterp.html',1,'ThunderEgg::Schur']]],
   ['trilinintp',['TriLinIntp',['../classThunderEgg_1_1GMG_1_1TriLinIntp.html',1,'ThunderEgg::GMG']]]
--- docs/develop-wip/doc/html/search/classes_7.js
+++ docs/develop-wip/doc/html/search/classes_7.js
@@ -9,7 +9,6 @@ var searchData=
   ['ifacetype',['IfaceType',['../classThunderEgg_1_1Schur_1_1IfaceType.html',1,'ThunderEgg::Schur']]],
   ['ilcfinetocoarsemetadata',['ILCFineToCoarseMetadata',['../structGMG_1_1Schur_1_1ILCFineToCoarseMetadata.html',1,'GMG::Schur']]],
   ['interlevelcomm',['InterLevelComm',['../classThunderEgg_1_1GMG_1_1InterLevelComm.html',1,'ThunderEgg::GMG::InterLevelComm&lt; D &gt;'],['../classGMG_1_1Schur_1_1InterLevelComm.html',1,'GMG::Schur::InterLevelComm&lt; D &gt;']]],
-  ['interlevelcommexception',['InterLevelCommException',['../structThunderEgg_1_1GMG_1_1InterLevelCommException.html',1,'ThunderEgg::GMG']]],
   ['interpolator',['Interpolator',['../classThunderEgg_1_1GMG_1_1Interpolator.html',1,'ThunderEgg::GMG']]],
   ['interpolator_3c_203_20_3e',['Interpolator&lt; 3 &gt;',['../classThunderEgg_1_1GMG_1_1Interpolator.html',1,'ThunderEgg::GMG']]],
   ['iterator',['Iterator',['../classThunderEgg_1_1Side_1_1Range_1_1Iterator.html',1,'ThunderEgg::Side&lt; D &gt;::Range::Iterator'],['../classThunderEgg_1_1Orthant_1_1Range_1_1Iterator.html',1,'ThunderEgg::Orthant&lt; D &gt;::Range::Iterator']]]
