c7e1e70cb26305d2f3b3517e12e2c0530e9de29d
Travis CI <travis@travis-ci.org>
2020-08-14 19:51:47 +0000

    Deploy code docs to GitHub Pages Travis build: 197
    Commit: 739466a18abfc09c4fddc4ca1ce07277aed4004e

--- docs/develop-wip/doc/html/search/functions_d.js
+++ docs/develop-wip/doc/html/search/functions_d.js
@@ -3,7 +3,7 @@ var searchData=
   ['p4estdomgen',['P4estDomGen',['../classThunderEgg_1_1P4estDomGen.html#afe952d7c20a1a7f4edec79e530970584',1,'ThunderEgg::P4estDomGen']]],
   ['patchinfo',['PatchInfo',['../structThunderEgg_1_1PatchInfo.html#acf21148ec598694ab3c9c159825c8b2b',1,'ThunderEgg::PatchInfo']]],
   ['patchoperator',['PatchOperator',['../classThunderEgg_1_1PatchOperator.html#af654d22705733cd74a6c0a3ef0e0f49b',1,'ThunderEgg::PatchOperator']]],
-  ['patchsolver',['PatchSolver',['../classThunderEgg_1_1PatchSolver.html#a13fad2d7723ab9e0ba000688d38b2878',1,'ThunderEgg::PatchSolver']]],
+  ['patchsolver',['PatchSolver',['../classThunderEgg_1_1PatchSolver.html#a1b3a09f8dc2b507b7acbbff3d2d4871e',1,'ThunderEgg::PatchSolver']]],
   ['polychebprec',['PolyChebPrec',['../classThunderEgg_1_1Schur_1_1PolyChebPrec.html#af46ded193373c4e386bb954e15b850ba',1,'ThunderEgg::Schur::PolyChebPrec']]],
   ['prepcoarser',['prepCoarser',['../classThunderEgg_1_1GMG_1_1Cycle.html#aafbf30ac50b07440181f34fc29301807',1,'ThunderEgg::GMG::Cycle']]],
   ['prepfiner',['prepFiner',['../classThunderEgg_1_1GMG_1_1Cycle.html#a7e0e1e63f0d98cc46c17ada5b8a4dbac',1,'ThunderEgg::GMG::Cycle']]]
