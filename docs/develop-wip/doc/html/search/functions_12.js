6533a7baddbe64788fd03086c5588ad88bd8939e
Travis CI <travis@travis-ci.org>
2020-09-30 21:05:19 +0000

    Deploy code docs to GitHub Pages Travis build: 275
    Commit: e2a42cae6c36d333237ffe5792cb31e7ba2dfb1d

--- docs/develop-wip/doc/html/search/functions_12.js
+++ docs/develop-wip/doc/html/search/functions_12.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['valvector',['ValVector',['../classThunderEgg_1_1ValVector.html#aa027d27acfdbf9b7220f0750dd16d89e',1,'ThunderEgg::ValVector']]],
+  ['valvector',['ValVector',['../classThunderEgg_1_1ValVector.html#a5a4a91766738b5e93e0df4c4a118ac68',1,'ThunderEgg::ValVector']]],
   ['valvectorgenerator',['ValVectorGenerator',['../classThunderEgg_1_1Schur_1_1ValVectorGenerator.html#a8bd27eb18a4509c8ec6e930644340fec',1,'ThunderEgg::Schur::ValVectorGenerator::ValVectorGenerator()'],['../classThunderEgg_1_1ValVectorGenerator.html#a3f5c7c04f7ad67a1fda3d38abce64e1a',1,'ThunderEgg::ValVectorGenerator::ValVectorGenerator()']]],
   ['vcycle',['VCycle',['../classThunderEgg_1_1GMG_1_1VCycle.html#a928112bbf06b91b9bb7d95cdf7dfa2b5',1,'ThunderEgg::GMG::VCycle']]],
   ['veclocaldatamanager',['VecLocalDataManager',['../classThunderEgg_1_1PETSc_1_1VecLocalDataManager.html#a692c30f7f67cfebbf7412abe2b72b6a9',1,'ThunderEgg::PETSc::VecLocalDataManager']]],
-  ['vector',['Vector',['../classThunderEgg_1_1Vector.html#a968a65cf78c80647560633df336cb9e6',1,'ThunderEgg::Vector']]],
+  ['vector',['Vector',['../classThunderEgg_1_1Vector.html#a75cddb80e98a26bab143942348ea5156',1,'ThunderEgg::Vector']]],
   ['vecwrapper',['VecWrapper',['../classThunderEgg_1_1PETSc_1_1VecWrapper.html#aba6c27729aa20a4dddfee1da175d0bd5',1,'ThunderEgg::PETSc::VecWrapper']]],
   ['vecwrappergenerator',['VecWrapperGenerator',['../classThunderEgg_1_1Schur_1_1VecWrapperGenerator.html#a6bd792233db9f60972c935243ec64eb3',1,'ThunderEgg::Schur::VecWrapperGenerator']]],
   ['visit',['visit',['../classThunderEgg_1_1GMG_1_1Cycle.html#af3981b8fcda2eb69e76e06e406e2e444',1,'ThunderEgg::GMG::Cycle::visit()'],['../classThunderEgg_1_1GMG_1_1VCycle.html#a0af45a7e2f45bff31277228e51160359',1,'ThunderEgg::GMG::VCycle::visit()'],['../classThunderEgg_1_1GMG_1_1WCycle.html#a360caa2bcd0984390760c756821ecf16',1,'ThunderEgg::GMG::WCycle::visit()']]],
