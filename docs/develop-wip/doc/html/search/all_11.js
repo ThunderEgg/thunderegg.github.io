4590940a30e5a80730bea2431a0bb7471077c9bb
Travis CI <travis@travis-ci.org>
2020-10-06 19:24:28 +0000

    Deploy code docs to GitHub Pages Travis build: 293
    Commit: d0f5899957f2b97b31254291d70307e801d2f671

--- docs/develop-wip/doc/html/search/all_11.js
+++ docs/develop-wip/doc/html/search/all_11.js
@@ -4,7 +4,7 @@ var searchData=
   ['petsc',['PETSc',['../namespaceThunderEgg_1_1PETSc.html',1,'ThunderEgg']]],
   ['schur',['Schur',['../namespaceThunderEgg_1_1Schur.html',1,'ThunderEgg']]],
   ['thunderegg',['ThunderEgg',['../namespaceThunderEgg.html',1,'']]],
-  ['timer',['Timer',['../classThunderEgg_1_1Timer.html',1,'ThunderEgg::Timer'],['../classThunderEgg_1_1PatchSolver.html#a984d15ea55e0a00fd778b8c0fccde3c8',1,'ThunderEgg::PatchSolver::timer()'],['../classThunderEgg_1_1Timer.html#ab75138b88f954a785afc99a10f435152',1,'ThunderEgg::Timer::Timer()']]],
+  ['timer',['Timer',['../classThunderEgg_1_1Timer.html',1,'ThunderEgg::Timer'],['../classThunderEgg_1_1Timer.html#ab75138b88f954a785afc99a10f435152',1,'ThunderEgg::Timer::Timer()'],['../classThunderEgg_1_1PatchSolver.html#a984d15ea55e0a00fd778b8c0fccde3c8',1,'ThunderEgg::PatchSolver::timer()']]],
   ['tne',['tne',['../classThunderEgg_1_1Orthant.html#a23ec121b1b27181ca29fba97922b3d89',1,'ThunderEgg::Orthant']]],
   ['tnw',['tnw',['../classThunderEgg_1_1Orthant.html#a6ed0c1f674e06fd37d570e136349e35c',1,'ThunderEgg::Orthant']]],
   ['top',['top',['../classThunderEgg_1_1Side.html#a517a8a05674bf0a151493e6151cf169f',1,'ThunderEgg::Side']]],
