a879225c7582f71895c40f6226d3cde66f4ebc5f
Travis CI <travis@travis-ci.org>
2021-02-22 23:49:28 +0000

    Deploy code docs to GitHub Pages Travis build: 354
    Commit: 10595f52ecaa0d60c0af261eb80054f6a04ca5ed

--- docs/develop-wip/doc/html/search/classes_d.js
+++ docs/develop-wip/doc/html/search/classes_d.js
@@ -5,7 +5,7 @@ var searchData=
   ['sidetypepiinfo',['SideTypePiinfo',['../classThunderEgg_1_1Schur_1_1Interface_1_1SideTypePiinfo.html',1,'ThunderEgg::Schur::Interface']]],
   ['smoother',['Smoother',['../classThunderEgg_1_1GMG_1_1Smoother.html',1,'ThunderEgg::GMG']]],
   ['solver',['Solver',['../classThunderEgg_1_1Iterative_1_1Solver.html',1,'ThunderEgg::Iterative']]],
-  ['starpatchoperator',['StarPatchOperator',['../classThunderEgg_1_1VarPoisson_1_1StarPatchOperator.html',1,'ThunderEgg::VarPoisson::StarPatchOperator&lt; D &gt;'],['../classThunderEgg_1_1Poisson_1_1StarPatchOperator.html',1,'ThunderEgg::Poisson::StarPatchOperator&lt; D &gt;']]],
+  ['starpatchoperator',['StarPatchOperator',['../classThunderEgg_1_1Poisson_1_1StarPatchOperator.html',1,'ThunderEgg::Poisson::StarPatchOperator&lt; D &gt;'],['../classThunderEgg_1_1VarPoisson_1_1StarPatchOperator.html',1,'ThunderEgg::VarPoisson::StarPatchOperator&lt; D &gt;']]],
   ['stencilhelper',['StencilHelper',['../classThunderEgg_1_1Poisson_1_1StencilHelper.html',1,'ThunderEgg::Poisson']]],
   ['stencilhelper2d',['StencilHelper2d',['../classThunderEgg_1_1Poisson_1_1StencilHelper2d.html',1,'ThunderEgg::Poisson']]]
 ];
